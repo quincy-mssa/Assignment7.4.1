ba6a5819bb36b170b45796101d9b7ee0fe000bb4
Quincy Miller <quincy.m.miller@outlook.com>
2024-12-19 17:01:59 -0500

    Add project files.

---
+++ ParkingSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7._4._1
+{
+    internal class ParkingSystem
+    {
+        private bool[] bigSlots;
+        private bool[] mediumSlots;
+        private bool[] smallSlots;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            bigSlots = new bool[big];
+            mediumSlots = new bool[medium];
+            smallSlots = new bool[small];
+        }
+
+        public bool AddCar(int carType)
+        {
+            bool[] targetArray = carType switch
+            {
+                1 => bigSlots,
+                2 => mediumSlots,
+                3 => smallSlots,
+                _ => null
+            };
+
+            if (targetArray == null) return false;
+
+            for (int i = 0; i < targetArray.Length; i++)
+            {
+                if (!targetArray[i])
+                {
+                    targetArray[i] = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment7._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+
+            Console.WriteLine(parkingSystem.AddCar(1));
+            Console.WriteLine(parkingSystem.AddCar(2));
+            Console.WriteLine(parkingSystem.AddCar(3));
+            Console.WriteLine(parkingSystem.AddCar(1));
+
+        }
+    }
+}
